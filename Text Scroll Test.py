af3e7458315b89fe5b332ad513431a31ab7ec113
RanFernaj <124270484+RanFernaj@users.noreply.github.com>
2024-01-28 00:21:02 +0000

    SCROLL TEXT WORKS WOOOO (using classes) in the text scroll. py

--- Text Scroll Test.py
+++ Text Scroll Test.py
@@ -14,9 +14,9 @@ timer = 0
 messages = ["Check this sick message",
             "This is another message!",
             "Isn\'t programming FUN :)))))))))))))"]
-# snip = font.render("", True, "white")
+
 counter = 0
-speed = 10  # Higher means slower speeds
+speed = 10
 active_message = 0
 message = messages[active_message]
 done = False
@@ -29,7 +29,27 @@ def show_text(text, location):
     render_text = font.render(display_text, True, pygame.Color("coral"))
     screen.blit(render_text, (text_location.x, text_location.y))
 
-# def scroll_text():
+
+class ScrollText:
+    def __init__(self, text_, speed_, delta_time):
+        self.text = text_
+        self.dt = delta_time
+        self.speed = speed_
+        # Setting the counter
+        self.counter = 0
+        self.counter_rounded = 0
+        self.snip = font.render("", True, "white")
+        self.screen = screen
+
+    def update(self):
+        if self.counter < len(self.text):
+            self.counter += (self.dt * self.speed)
+            self.counter_rounded = round(self.counter)
+
+    def render(self, ):
+        self.update()
+        self.snip = font.render(self.text[0:counter_rounded], True, "white")
+        self.screen.blit(self.snip, (10, 500))
 
 
 def render():
@@ -39,19 +59,18 @@ def render():
     show_text(f'FPS: {str(int(clock.get_fps()))}', (10, 0))
     show_text(f'Delta Time: {str(dt)}', (500, 0))
     show_text(f'Timeer: {str(int(timer))}', (700, 0))
-    show_text(f'Counter: {str(counter_rounded)}', (1000, 0))
+
+    text1.render()
 
     pygame.display.flip()
 
 
-
-
+text1 = ScrollText("Check this sick message", 10, dt)
 
 while running:
     # fill the screen with a color to wipe away anything from last frame
     dt = clock.tick(60) / 1000
     timer += dt
-    # timer_ = round(timer)
     if counter < len(message):
         counter += dt * speed
         counter_rounded = round(counter)
@@ -65,8 +84,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # snip = font.render(message[0:counter], True, "white")
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RETURN] and done and active_message < len(messages) - 1 and counter > 0:
